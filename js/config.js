9b220298b49a325cfb4fdc151aac3ffafa8f89bc
zivigder <87516215+zivigder@users.noreply.github.com>
2025-04-05 13:16:22 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_wO4ie8nv3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '9tv6nfl5fsd4h5ji7rlf9o0cs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
